c1a85c1ecb85123210fd60632ad95b240f929b5c
Carlosqchao <carlosruanoramos@gmail.com>
2025-04-12 23:10:00 +0100

    dev: Added NSFW detector

--- src/nsfw-backend/src/index.ts
+++ src/nsfw-backend/src/index.ts
@@ -9,27 +9,22 @@ import cors from 'cors';
 const app = express();
 const port = 3000;
 
-// Configuración de Multer para subir archivos
 const upload = multer({ dest: '../uploads' });
 
-// Usar CORS para permitir solicitudes desde cualquier origen
 app.use(cors());
 
-// Ruta para comprobar el archivo PDF y verificar si contiene contenido NSFW
 // @ts-ignore
 app.post('/check-nsfw', upload.single('file'), async (req: Request, res: Response) => {
   const pdfPath = req.file?.path;
   const outputDir = path.join(__dirname, '../output');
   const outputPrefix = path.join(outputDir, 'page');
 
-  // Crear el directorio de salida si no existe
   if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
 
   if (!pdfPath) {
     return res.status(400).json({ error: 'No file uploaded' });
   }
 
-  // Comando para convertir PDF a imágenes JPG
   const cmd = `pdftoppm -jpeg -scale-to 1024 "${pdfPath}" "${outputPrefix}"`;
 
   // @ts-ignore
@@ -41,7 +36,6 @@ app.post('/check-nsfw', upload.single('file'), async (req: Request, res: Respons
     }
     console.log('stdout:', stdout);
 
-    // Llamar al script Python de detección NSFW
     const python = spawn('python', ['src/nsfw-backend/src/nsfw_check.py', outputDir]);
 
     let output = '';
@@ -55,7 +49,6 @@ app.post('/check-nsfw', upload.single('file'), async (req: Request, res: Respons
   });
 });
 
-// Iniciar el servidor en el puerto 3000
 app.listen(port, () => {
   console.log(`🚀 Server is running on http://localhost:${port}`);
 });
