14d262c3a3f6569169396463b5207ec8d59ba94d
juanfradelrm <juanfradelrm@gmail.com>
2025-05-01 17:14:57 +0200

    (feat)wip: Profiles options in progress.

--- backend/src/services/user-store.ts
+++ backend/src/services/user-store.ts
@@ -40,12 +40,12 @@ export class UserStoreService {
 
   user: IUser | null = null;
   getUserAge(): number {
-    this.getUser().subscribe(user =>
-    this.user = user);
-
-    if (this.user || this.user!.birthdate) {
+    this.getUser().subscribe(user => {
+      this.user = user;
+    })
+    if (!this.user || !this.user.birthdate) {
       return 0;
     }
-    return this.calculateAge(this.user!.birthdate);
+    return this.calculateAge(this.user.birthdate);
   }
 }
