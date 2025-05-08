ab8f3c59e36d7ca3bbd79e366fadc95478a79cbd
Carlosqchao <carlosruanoramos@gmail.com>
2025-05-08 17:19:38 +0100

    Dev: Search-page & Upload-form Responsive!!

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -103,19 +103,8 @@ export class SearchPageComponent implements OnInit {
 
 
   dropdownVisible = false;
-  sortAscending = true;
-
   toggleDropdown() {
     this.dropdownVisible = !this.dropdownVisible;
-  }
-
-  sortBy(option: string) {
-    if (option === 'date') {
-      this.loadComicsOrderedByDate();
-    } else if (option === 'rating') {
-      this.loadComicsOrderedByRating();
-    }
-    this.dropdownVisible = false;
   }
   sortByRatingAscending = false;
   loadComicsOrderedByRating() {
@@ -123,7 +112,7 @@ export class SearchPageComponent implements OnInit {
       this.comics = orderedComics;
     });
 
-    // Alterna el orden para la próxima vez que se llame
+
     this.sortByRatingAscending = !this.sortByRatingAscending;
   }
 
@@ -191,5 +180,28 @@ export class SearchPageComponent implements OnInit {
     });
   }
 
+  maxComicsPerPage = 15;
+  currentPage = 1;
 
+  get totalPages(): number {
+    return Math.ceil(this.comics.length / this.maxComicsPerPage);
+  }
+
+  get paginatedComics() {
+    const start = (this.currentPage - 1) * this.maxComicsPerPage;
+    const end = start + this.maxComicsPerPage;
+    return this.comics.slice(start, end);
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
 }
